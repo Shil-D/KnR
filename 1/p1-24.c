07a37e433e9d35d3afeb5ace4b2515065fc9b43c
Shil.D <Shil.D@yandex.ru>
2017-06-22 21:06:22 +0300

    p1-24.c fix

--- 1/p1-24.c
+++ 1/p1-24.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#define MAXLEN 1000
 
+int index;
+int arr[MAXLEN];
+int check(char d);
 
 main(){
 	int c, inC, inDQ, inQ, exit, result, get;
-	int p1, p2, p3;
+	index = 0;
 	inC = inQ = inDQ = exit = 0;
 	result = 1;
 	get = 1;
-	p1 = p2 = p3 = 0;
+	
 	while (!exit){
 		if (get) c = getchar();
 		else get = 1;
@@ -27,12 +31,12 @@ main(){
 			else if (c == '\''){
 				inQ = 1;
 			}
-			else if (c == '(') p1++;
-			else if (c == '{') p2++;
-			else if (c == '[') p3++;
-			else if (c == ')') p1--;
-			else if (c == '}') p2--;
-			else if (c == ']') p3--;
+			if (result == 1){
+				if (c == '(' || c == '{'|| c == '[') {  arr[index] = c; index++;}
+				else if (c == ')') result = check('(');
+				else if (c == '}') result = check('{');
+				else if (c == ']') result = check('[');
+			}
 		}
 		else if (inC){
 			if (c == '*'){
@@ -53,11 +57,19 @@ main(){
 			if (c == '"') inDQ = 0;
 		}
 		if (c != EOF) putchar(c);
-		if (p1 < 0 || p2 < 0 || p3 < 0) result = 0;
 	}
-	if (result == 0) printf(":(\n");
-	else if (p1 == 0 && p2 == 0 && p3 == 0) printf("OK\n");
-	else printf(":(");
+	if (result == 0 || index != 0) printf(":(\n");
+	else printf("OK\n");
 	
 	return 0;
+}
+
+
+int check(char d){
+	if (index == 0) return 0;
+	else if (arr[index-1] == d){
+		index--;
+		return 1;
+	}
+	else return 0;
 }
