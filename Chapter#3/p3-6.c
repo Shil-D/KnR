901a93f90f4bb4b17a718eba23120a309ae2b015
Shil.D <Shil.D@yandex.ru>
2017-08-10 20:12:18 +0300

    p3-6

---
+++ Chapter#3/p3-6.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+void itoa(int n, char s[], int b);
+void reverse(char s[]);
+
+main(){
+    int n = -4235;
+    char s[20];
+    
+    printf("N = %d\n", n);
+    itoa(n,s, 8);
+    printf("Str N = %s\n", s);
+
+    return 0;
+}
+
+void itoa(int n, char s[], int b){
+    int i;
+    unsigned int n2;
+    
+    if (n < 0) n2 = -n;
+    else n2 = n;
+    i=0;
+    do{
+        s[i++] = n2 % 10 + '0';
+    } while ((n2 /= 10) > 0);
+    if ( n < 0) s[i++] = '-';
+    for (; i < b; i++)
+        s[i] = ' ';
+    s[i] = '\0';
+    reverse(s);
+}
+void reverse(char s[]){
+    int c,i,j;
+
+    for (i = 0, j = strlen(s) - 1; i < j; i++, j--){
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
