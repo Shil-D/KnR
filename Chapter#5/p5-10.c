451ac0e0f3ee560690d6e184fb83b5ed41940063
Shil.D <Shil.D@yandex.ru>
2017-10-04 20:35:39 +0300

    p5-10

--- Chapter#5/p5-10.c
+++ Chapter#5/p5-10.c
@@ -34,38 +34,37 @@ main(int argc, char *argv[]){
         type = (*++argv)[0];
         if (isdigit(type))
             push(atof(*argv));
-        switch(type){
-            case '+':
-                push(pop() + pop());
-                break;
-            case '*':
-                push(pop() * pop());
-                break;
-            case '-':
-                op2 = pop();
-                push(pop() - op2);
-                break;
-            case '/':
-                op2 = pop();
-                if (op2 != 0.0)
-                    push(pop() / op2);
-                else 
-                    printf("error: zero divisor\n");
-                break;
-            case '%':
-                op2 = pop();
-                if (op2 != 0.0)
-                    push( (int)pop() % (int)op2);
-                else 
-                    printf("error: zero divisor\n");
-                break;
-            case '\n':
-                printf("\t%.8g\n", pop());
-                break;
-            default:
-                printf("error: unknown command %s\n", s);
-                break;
-        }
+        else
+            switch(type){
+                case '+':
+                    push(pop() + pop());
+                    break;
+                case '*':
+                    push(pop() * pop());
+                    break;
+                case '-':
+                    op2 = pop();
+                    push(pop() - op2);
+                    break;
+                case '/':
+                    op2 = pop();
+                    if (op2 != 0.0)
+                        push(pop() / op2);
+                    else 
+                        printf("error: zero divisor\n");
+                    break;
+                case '%':
+                    op2 = pop();
+                    if (op2 != 0.0)
+                        push( (int)pop() % (int)op2);
+                    else 
+                        printf("error: zero divisor\n");
+                    break;
+                default:
+                    printf("error: unknown command %s\n", *argv);
+                    break;
+            }
     }
+    printf("\t%.8g\n", pop());
     return 0;
 }
