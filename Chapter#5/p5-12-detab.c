aac965b84c0ae813f9a0fef97e0c82cc2203088d
Shil.D <Shil.D@yandex.ru>
2017-10-14 18:03:49 +0300

    p5-12

--- Chapter#5/p5-12-detab.c
+++ Chapter#5/p5-12-detab.c
@@ -7,19 +7,20 @@ main(int argc, char *argv[]){
 	int tab = TAB;
 	int starttab = 0;
 
-	i=0;
-	while (--argc > 0)
-		switch (c = *++argv[0]){
+    i=0;
+	while (--argc > 0){
+        c = **++argv;
+		switch (c){
             case '+':
-                tab = atoi(argv[1]);
+                tab = atoi(*argv + 1);
                 break;
             case '-':
-                starttab = atoi(argv[1]);
+                starttab = atoi(*argv+1);
                 break;
             default:
                 printf("Illegal argument\n");
                 return 0;
-        }
+        }}
 		
 	while ((c = getchar()) != EOF){
 		++i;
@@ -27,8 +28,8 @@ main(int argc, char *argv[]){
 			i = 0;
 		}
 		
-		if (c == '\t') 
-            while ( (i - starttab) % tab != 0) {
+		if (c == '\t' && i > starttab)
+            while ( i % tab != 0) {
                 putchar(' ');
                 ++i;
             }
--- Chapter#5/p5-12-entab.c
+++ Chapter#5/p5-12-entab.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define TAB 4
 
-main(){
-	int i,c;
+main(int argc, char *argv[]){
+	int i,j,c, d;
 	int tab = TAB;
 	int starttab = 0;
 
-	i=0;
+	d=0;
 	while (--argc > 0)
-		switch (c = *++argv[0]){
+		switch (c = **++argv){
             case '+':
-                tab = atoi(argv[1]);
+                tab = atoi(*argv+1);
                 break;
             case '-':
-                starttab = atoi(argv[1]);
+                starttab = atoi(*argv+1);
                 break;
             default:
-                printf("Illegal argument\n");
+                printf("Illegal arguments\n");
                 return 0;
         }
 	
 	while ((c = getchar()) != EOF){
-		if (c == '\n') i = 0;
-		if (c == ' ') {
+		d++;
+		if (c == '\n') d = 0;
+		if (c == ' ' && d > starttab) {
 			i++ ;
-			if ((i-starttab) % tab == 0) {
+			if (i % tab == 0) {
 				i = 0;
 				putchar('\t');
 			}
